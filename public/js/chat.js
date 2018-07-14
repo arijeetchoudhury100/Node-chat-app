de26d0bd11a8d70ba8f68644a216a8b778c7ac37
Arijeet Choudhury <arijeetchoudhury100@gmail.com>
2018-07-14 20:27:44 +0530

    make messages private to room

--- public/js/chat.js
+++ public/js/chat.js
@@ -66,7 +66,6 @@ jQuery('#message-form').on('submit',function(e){
 
   var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage',{
-    from : 'user',
     text : messageTextbox.val()
   },function(){
     messageTextbox.val('');
